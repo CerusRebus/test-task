5715c101e86bfc4d866801c9e9c6eae71aa53692
CerusRebus <donik.donik6699@gmail.com>
2022-07-14 09:36:29 +0300

    test task for company V 2.0

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const iconMenu = document.querySelector('.menu_icon');
-const menuBody = document.querySelector('.menu_body');
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
 if (iconMenu) {
     iconMenu.addEventListener("click", function (e) {
         document.body.classList.toggle('_lock');
